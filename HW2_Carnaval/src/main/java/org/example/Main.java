64272acc1b850bd57928546f455cdff2b07d6df9
Anton <strangepear@mail.ru>
2026-03-05 00:40:57 +0300

    working version

--- HW2_Carnaval/src/main/java/org/example/Main.java
+++ HW2_Carnaval/src/main/java/org/example/Main.java
@@ -1,6 +1,14 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Carnaval carnaval = new Carnaval();
+        PartisipantFactory factory = new PartisipantFactory();
+
+        System.out.println("\nЗагружено участников из YAML: " + factory.getPartCount());
+
+        for (int i = 0; i < factory.getPartCount(); i ++){
+            carnaval.addParticipant(factory.createPartisipant());
+        }
+        carnaval.dance();
     }
 }
